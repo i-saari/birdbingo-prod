a3bb999965a5b6878501801b129421e5441df440
Ian Saari <ian.saari1@ucalgary.ca>
2024-01-26 19:43:08 -0700

    changed roboto font import to cdn

--- src/bingoTheme.tsx
+++ src/bingoTheme.tsx
@@ -1,9 +1,5 @@
 import {createTheme, Shadows} from '@mui/material/styles';
 import {lightBlue, lightGreen} from '@mui/material/colors';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 export const theme = createTheme({
     palette: {
